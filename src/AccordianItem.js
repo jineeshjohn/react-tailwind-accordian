76eb777a4cda80234e501cd02276fa2c134dd307
jineeshjohn <jineeshjohn@gmail.com>
2022-12-01 17:36:12 +1100

    Add basic design

--- src/AccordianItem.js
+++ src/AccordianItem.js
@@ -10,8 +10,13 @@ const AccordionHeading = styled.div`
   padding: 10px;
 `;
 
-const AccordionItem = ({ content }) => {
-  const [open, setOpen] = useState(false);
+const AccordionItem = ({
+  header,
+  children,
+  className = '',
+  isOpen = false,
+}) => {
+  const [open, setOpen] = useState(isOpen);
   const [contentMaxHeight, setContentMaxHeight] = useState(0);
   const ref = useRef();
 
@@ -30,26 +35,45 @@ const AccordionItem = ({ content }) => {
   const { scY, y, ...props } = useSpring({
     scY: open ? -1 : 1,
     // y: open ? 0 : -contentMaxHeight,
-    opacity: open ? 1 : 0,
     maxHeight: open ? `${contentMaxHeight}px` : '0px',
     config: { duration: 300 },
   });
-
   return (
     <div>
-      <AccordionHeading onClick={() => setOpen(!open)}>
-        <div>{content.title}</div>
-        <animated.div
-          style={{
-            padding: '5px',
-            transform: scY.interpolate((scY) => `scaleY(${scY})`),
-          }}
-          height="16px"
-          pl={2}
+      <h2 id="accordion-collapse-heading-1" onClick={() => setOpen(!open)}>
+        <button
+          type="button"
+          className={`flex ${className} items-center justify-between w-full p-3 font-medium text-left  border border-b-0 border-gray-200   focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 bg-gray-200 hover:bg-gray-300 dark:hover:bg-gray-800`}
+          data-accordion-target="#accordion-collapse-body-1"
+          aria-expanded="true"
+          aria-controls="accordion-collapse-body-1"
         >
-          ˇ
-        </animated.div>
-      </AccordionHeading>
+          <span>{header}</span>
+          <animated.span
+            style={{
+              padding: '5px',
+              transform: scY.interpolate((scY) => `scaleY(${scY})`),
+            }}
+            height="16px"
+            pl={2}
+          >
+            <svg
+              data-accordion-icon
+              className="w-6 h-6 rotate-180 shrink-0"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </animated.span>
+        </button>
+      </h2>
+
       <animated.div
         ref={ref}
         style={{
@@ -57,7 +81,7 @@ const AccordionItem = ({ content }) => {
           ...props,
         }}
       >
-        <div pb={1}>{content.text}</div>
+        {children}
       </animated.div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -15,13 +15,190 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1 className="text-3xl font-bold underline text-red-600">
-          Hello world!
-        </h1>
-        {accordionContent.map((item, id) => (
-          <AccordionItem key={id} id={id} content={item} />
-        ))}
+      <div className="border border-grey-600 m-5 p-5  ">
+        <div className="container p-2 mx-auto">
+          <div className="flex flex-row flex-wrap py-4">
+            <aside className="w-full sm:w-1/3 md:w-1/4 px-2">
+              <AccordionItem
+                header="Description"
+                classNameName="rounded-t-xl"
+                isOpen={true}
+              >
+                <div
+                  id="accordion-open-body-1"
+                  aria-labelledby="accordion-open-heading-1"
+                >
+                  <div className="p-5 font-light border ">
+                    <p className="mb-2  dark:text-gray-400">
+                      Lorem Ipsum is simply dummy text of the printing and
+                      typesetting industry. Lorem Ipsum has been the industry's
+                      standard dummy text ever since the 1500s, when an unknown
+                      printer took a galley of type and scrambled it to make a
+                      type specimen book. It has survived not only five
+                      centuries, but also the leap into electronic typesetting,
+                      remaining essentially unchanged. It was popularised in the
+                      1960s with the release of Letraset sheets containing Lorem
+                      Ipsum passages, and more recently with desktop publishing
+                      software like Aldus PageMaker including versions of Lorem
+                      Ipsum.
+                    </p>
+                  </div>
+                </div>
+              </AccordionItem>
+              <AccordionItem header="Details" isOpen={true}>
+                <ul className="p-5 max-w-md   divide-gray-200 dark:divide-gray-700 border ">
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">ProjectId</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        ERF121013
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Vintage</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        2022
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Location</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        New South Wales
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Methodology</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        Human-Induced Regeneration
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </AccordionItem>
+              <AccordionItem header="Contract" isOpen={true}>
+                <ul className="p-5 max-w-md   divide-gray-200 dark:divide-gray-700 border rounded-b-xl">
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">ProjectId</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        ERF121013
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Vintage</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        2022
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Location</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        New South Wales
+                      </div>
+                    </div>
+                  </li>
+                  <li className="pb-3 sm:pb-4">
+                    <div className="flex items-center space-x-4">
+                      <div className="flex-1 min-w-0">Methodology</div>
+                      <div className="inline-flex items-center text-base font-semibold  dark:text-white">
+                        Human-Induced Regeneration
+                      </div>
+                    </div>
+                  </li>
+                </ul>
+              </AccordionItem>
+            </aside>
+            <main role="main" className="w-full sm:w-2/3 md:w-3/4 pt-1 px-2">
+              <AccordionItem header="Additional Listings" isOpen={true}>
+                <table className="w-full text-sm text-left  dark:text-gray-400">
+                  <thead className="text-xs  uppercase dark:text-gray-400">
+                    <tr>
+                      <td scope="col" className="py-3 px-6">
+                        Quantity (Tonnes CO2)
+                      </td>
+                      <td scope="col" className="py-3 px-6">
+                        Unit Price (A$DC)
+                      </td>
+                      <td scope="col" className="py-3 px-6">
+                        Total Price (A$DC)
+                      </td>
+                      <td>&nbsp;</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="bg-white">
+                      <td
+                        scope="row"
+                        className="py-4 px-6 font-medium  whitespace-nowrap "
+                      >
+                        5.0
+                      </td>
+                      <td className="py-4 px-6">45.00</td>
+                      <td className="py-4 px-6">225.00</td>
+                      <td className="py-4 px-6">b</td>
+                    </tr>
+                    <tr className="bg-white">
+                      <td
+                        scope="row"
+                        className="py-4 px-6 font-medium  whitespace-nowrap "
+                      >
+                        20.0
+                      </td>
+                      <td className="py-4 px-6">45.00</td>
+                      <td className="py-4 px-6">900.00</td>
+                      <td className="py-4 px-6">b</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </AccordionItem>
+              <br />
+
+              <AccordionItem header="Item Activity">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              </AccordionItem>
+              <br />
+
+              <AccordionItem
+                header="More From This Collection"
+                classNameName=""
+              >
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining
+                  essentially unchanged. It was popularised in the 1960s with
+                  the release of Letraset sheets containing Lorem Ipsum
+                  passages, and more recently with desktop publishing software
+                  like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              </AccordionItem>
+            </main>
+          </div>
+        </div>
       </div>
     );
   }
